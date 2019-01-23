61a6c271f09537379d0a5cd1ddb0ce650a803085
Tsubota, Tamara <tamara.tsubota@gmail.com>
2019-01-22 19:32:31 -0500

    Issue4 Add document dropdown to results page

--- results.php
+++ results.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html>
   <head>
+  <script src='results.js'></script>
     <title>Ambazonian Genocide Watch</title> <!-- TODO: come up with a better name? -->
     <link rel='stylesheet' href='init.css' />
   </head>
@@ -11,6 +12,17 @@
       <a id='nav_browse' href='browse.html'>Browse</a>
     </div>
     <div style='padding:1rem;'></div>
+    <div style='padding:1.5rem;'></div>
+    <select>
+      <option value=-1>All</option>
+      <option value=1>Photos</option>
+      <option value=2>Videos</option>
+      <option value=3>Audio Recordings</option>
+    </select>
+    <select onchange="change_province(this.value)">
+      <option value="Show all provinces">Show all provinces</option>
+      <script type='text/javascript'>province_dropdown();</script>
+    </select>
     <p>The province that was selected is: <?php echo $_GET["province"]; ?>.</p>
   </body>
 </html>
