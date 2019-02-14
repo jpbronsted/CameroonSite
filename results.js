1157de33237fb6ca4bfa9cf5b17db61d6af041a9
Tsubota, Tamara <tamara.tsubota@gmail.com>
2019-02-13 22:06:39 -0500

    Trying to view/download photos for #17
    Very unfinished code. Cannot figure out why it's broken.

--- results.js
+++ results.js
@@ -9,50 +9,80 @@ var config = {
 firebase.initializeApp(config);
 
 var storage = firebase.app().storage();
+var storage_ref = storage.ref();
 var db = firebase.firestore();
 
 function initialize(){
     var doctype = document.getElementById('doctype').value;
     var province = document.getElementById('province').value;
 
-    get_storage_bucket(doctype, province);
+    get_documents(doctype, province);
 }
 
-function get_storage_bucket(doctype, province){
-    var storage_url = 'gs://cameroonsite-5f7e2.appspot.com/';
+function get_documents(doctype, province){
+    var url = 'counties';
+    var documents_array = [];
+    var doc_url_array = [];
 
-    if(doctype === 'Photos') {
-        storage_url += 'Photos';
-
-        if(province !== 'all') {
-            storage_url += '/' + province;
-        }
-    } else if(doctype === 'Videos') {
-        storage_url += 'Videos';
-
-        if(province !== 'all') {
-            storage_url += '/' + province;
-        }
-    } else if(doctype === 'Audio Recordings') {
-        storage_url += 'Recordings';
-
-        if(province !== 'all') {
-            storage_url += '/' + province;
-        }
-    } else if( province !== 'all' ) {
-        storage_url += 'Photos/' + province;
-        storage = firebase.app().storage(storage_url)
-        display_documents();
-
-        storage_url += 'Videos/' + province;
-        storage = firebase.app().storage(storage_url)
-        display_documents();
-
-        storage_url += 'Recordings/' + province;
+    if(province !== 'all') {
+        url += '/'+province;
     }
 
-    storage = firebase.app().storage(storage_url)
-    display_documents();
+    var docRef = db.doc(url);
+
+    docRef.get().then( function(doc) {
+        if( doctype !== 'Show All Documents') {
+            if(doctype === 'Audio Recordings'){
+                doctype = 'Recordings';
+            }
+            documents_array = doc.get(doctype);
+            documents_array.forEach(function(document) {
+               storage_ref.child(document).getDownloadURL().then(function(url) {
+                 doc_url_array.push(url);
+                 document.getElementById('documents').innerHTML = doc_url_array[0];
+                 var xhr = new XMLHttpRequest();
+                 xhr.responseType = 'blob';
+                 xhr.onload = function(event) {
+                   var blob = xhr.response;
+                 };
+                 xhr.open('GET', url);
+                 xhr.send();
+
+                 document.getElementById('myimg').src = url;
+               }).catch(function(error) {
+                 // Handle any errors
+               });
+            });
+        } else {
+            documents_array = doc.get('Photos');
+            Array.prototype.push.apply(documents_array, doc.get('Videos'));
+            Array.prototype.push.apply(documents_array, doc.get('Recordings'));
+        }
+    })
+    .catch(function(error) {
+        console.log("Error getting documents: ", error);
+    });
+
+
+    if(doctype === 'Photos') {
+        url += 'Photos';
+
+        if(province !== 'all') {
+            url += '/' + province;
+        }
+    } else if(doctype === 'Videos') {
+        url += 'Videos';
+
+        if(province !== 'all') {
+            url += '/' + province;
+        }
+    } else if(doctype === 'Audio Recordings') {
+        url += 'Recordings';
+
+        if(province !== 'all') {
+            url += '/' + province;
+        }
+    }
 }
 
 function display_documents() {
--- results.php
+++ results.php
@@ -82,6 +82,8 @@ foreach($counties as $name) {
             </form>
             <p>The province that was selected is: <?php echo $province; ?>.</p>
             <p>The type that was selected is: <?php echo $type; ?>.</p>
+            <p id='documents'></p>
+            <img id='myimg' src='' alt=''>
         </div>
     </body>
 </html>
