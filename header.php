fe0b88a675a1549dee5a28c1b70ce7ef2ae0a326
jpbronsted <jamespbronsted@gmail.com>
2019-04-08 23:39:19 -0400

    added several new pages for future work, pursuant to #30

--- header.php
+++ header.php
@@ -38,6 +38,31 @@ if( !isset( $page ) )
                     <a class="nav-link" href='browse.php' class="<?php echo $page === 'browse'
                         ? 'active' : '';?>">Browse</a>
                 </li>
+
+                <li class="nav-item">
+                    <a class="nav-link" href='history.php' class="<?php echo $page === 'history'
+                        ? 'active' : '';?>">History</a>
+                </li>
+
+                <li class="nav-item">
+                    <a class="nav-link" href='genocide.php' class="<?php echo $page === 'genocide'
+                        ? 'active' : '';?>">Genocide</a>
+                </li>
+
+                <li class="nav-item">
+                    <a class="nav-link" href='refugees.php' class="<?php echo $page === 'refugees'
+                        ? 'active' : '';?>">Refugees</a>
+                </li>
+
+                <li class="nav-item">
+                    <a class="nav-link" href='news.php' class="<?php echo $page === 'news'
+                        ? 'active' : '';?>">News</a>
+                </li>
+
+                <li class="nav-item">
+                    <a class="nav-link" href='guestbook.php' class="<?php echo $page === 'guestbook'
+                        ? 'active' : '';?>">Guestbook</a>
+                </li>
             </ul>
         </nav>
     </head>
